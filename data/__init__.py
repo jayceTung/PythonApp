15b0d5159b3fab46c25c5cf45d626f5be5292d40
Super <minchao.dong@melot.cn>
2018-01-25 14:31:37 +0800

    Initial commit

---
+++ data/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : super
+# @Time    : 2018/1/25
+# @desc    :
---
+++ data/dict.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : super
+# @Time    : 2018/1/25
+# @desc    : data of dict
+
+if __name__ == '__main__':
+    dic = dict()
+    dic['dandy'] = 10
+    print(dic.keys())
+
+    print(len(dic), str(dic))
